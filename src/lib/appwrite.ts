9c8946e1e1a567cb2b5483fc2676ad52a603f658
danihermes <danielhn@gmail.com>
2024-02-11 12:42:55 -0500

    event handlers and live beat interface

---
+++ src/lib/appwrite.ts
@@ -0,0 +1,9 @@
+import { Client, Databases, Storage } from 'appwrite';
+
+export const client = new Client();
+export const databases = new Databases(client);
+export const storage = new Storage(client);
+
+client
+    .setEndpoint(import.meta.env.VITE_APPWRITE_ENDPOINT)
+    .setProject(import.meta.env.VITE_APPWRITE_PROJECT_ID);
---
+++ src/lib/events.ts
@@ -0,0 +1,37 @@
+import { databases } from "./appwrite";
+import { Models, ID } from "appwrite";
+import { LiveBeatEvent } from "@/types/events";
+
+export async function getEvents() {
+    const { documents } = await databases.listDocuments(import.meta.env.VITE_APPWRITE_EVENTS_DATABASE_ID, import.meta.env.VITE_APPWRITE_EVENTS_COLLECTION_ID);
+    return {
+        events: documents.map(mapDocumentToEvent)
+    }
+}
+
+export async function getEventById(eventId: string) {
+    const document = await databases.getDocument(import.meta.env.VITE_APPWRITE_EVENTS_DATABASE_ID, import.meta.env.VITE_APPWRITE_EVENTS_COLLECTION_ID, eventId  );
+    return {
+        event: mapDocumentToEvent(document)
+    }
+}
+
+export async function createEvent(event: Omit<LiveBeatEvent, '$id'>) {
+    const document = await databases.createDocument(import.meta.env.VITE_APPWRITE_EVENTS_DATABASE_ID, import.meta.env.VITE_APPWRITE_EVENTS_COLLECTION_ID, ID.unique(), event  );
+    return {
+        event: mapDocumentToEvent(document)
+    }
+}
+
+function mapDocumentToEvent(document: Models.Document) {
+    const event: LiveBeatEvent = {
+        $id: document.$id,
+        name: document.name,
+        location: document.location,
+        date: document.date,
+        imageFileId: document.imageFileId,
+        imageHeight: document.imageHeight,
+        imageWidth: document.imageWidth,
+    }
+    return event;
+}
